1eaf6c650de42e9d4d38a84c17875752dbc49bbb
Isaias Carlos <109770805+ICN153@users.noreply.github.com>
2024-08-29 21:08:59 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// console.log('teste de javascript');
+
+//seleção de elementos HTML
+
+var btnMenu = document.querySelector('.btn-menu');
+var menu = document.querySelector('.menu');
+
+//console.log(btnMenu);
+//console.log(menu);
+
+//Função (rotina) que vai manipular o menu
+
+function menuSite(){
+    // console.log('Função acionada');
+
+    //toggle -> alterar entre add e remover uma classe
+    menu.classList.toggle('menu-open');
+    btnMenu.classList.toggle('x');
+}
+
+//Evento
+//ap clicar no btnMenu será acionada a FUNÇÃO menuSite
+btnMenu.addEventListener('click', menuSite);
